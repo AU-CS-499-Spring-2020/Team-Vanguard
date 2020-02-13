0e96c80785eaddbae5bdf2bfe56894c94281b327
Nathaniel Redden <44184031+nwredden@users.noreply.github.com>
2020-02-13 13:03:12 -0500

    Initial Code for the project
    Created a basic framework for part of the app

---
+++ Code/BlackForkWetlandsApp/app/src/main/java/com/example/blackforkwetlandsapp/OrganismClass.java
@@ -0,0 +1,38 @@
+package com.example.blackforkwetlandsapp;
+
+public class OrganismClass {
+
+    private String name, description;
+    private int imageID;
+
+
+    public OrganismClass(String name, String description, int imageID) {
+        this.name = name;
+        this.description = description;
+        this.imageID = imageID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+}
